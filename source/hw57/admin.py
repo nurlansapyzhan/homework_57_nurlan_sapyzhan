9ca554fed476c7ae98c283d121288a4475f004a8
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-03-01 21:28:07 +0600

    Admin, Meta
    Зарегестрировал модели в админ панели. Добавил моделям метакласс

--- source/hw57/admin.py
+++ source/hw57/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from hw57.models import Type, Status, Task
+
+
 # Register your models here.
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('id', 'name')
+    fields = ('id', 'name')
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('id', 'name')
+    fields = ('id', 'name')
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'description', 'status', 'type', 'created_date', 'updated_date')
+    list_filter = ('id', 'summary', 'description', 'status', 'type')
+    search_fields = ('status', 'type', 'finish_date', 'updated_date')
+    fields = ('id', 'summary', 'description', 'status', 'type', 'created_date', 'updated_date')
+    readonly_fields = ('id', 'created_date', 'updated_date')
+
+
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Task, TaskAdmin)
--- source/hw57/models.py
+++ source/hw57/models.py
@@ -3,23 +3,69 @@ from django.db import models
 
 # Create your models here.
 class Type(models.Model):
-    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Тип задачи')
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Тип задачи'
+    )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Тип задачи'
+        verbose_name_plural = 'Типы задач'
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Статус задачи')
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Статус задачи'
+    )
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
 
 class Task(models.Model):
-    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name='Краткое описание')
-    description = models.TextField(max_length=254, null=True, verbose_name='Полное описание')
-    status = models.ForeignKey(to=Status, on_delete=models.PROTECT)
-    type = models.ForeignKey(to=Type, on_delete=models.PROTECT)
-    created_date = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
-    updated_date = models.DateTimeField(verbose_name='Дата и время обновления', auto_now=True)
+    summary = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name='Краткое описание'
+    )
+    description = models.TextField(
+        max_length=254,
+        null=True,
+        verbose_name='Полное описание'
+    )
+    status = models.ForeignKey(
+        to=Status,
+        on_delete=models.PROTECT
+    )
+    type = models.ForeignKey(
+        to=Type,
+        on_delete=models.PROTECT
+    )
+    created_date = models.DateTimeField(
+        verbose_name='Дата и время создания',
+        auto_now_add=True
+    )
+    updated_date = models.DateTimeField(
+        verbose_name='Дата и время обновления',
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'{self.summary}, {self.status}, {self.type}'
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
