65b216761a17c2fb9b22fbb10cafc2de8c21fd2b
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-03-01 21:11:02 +0600

    models and migrations
    Создал модели для типа, статуса и задачи. Создал миграции и применил их

--- source/hw57/models.py
+++ source/hw57/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Тип задачи')
+
+    def __str__(self):
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Статус задачи')
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name='Краткое описание')
+    description = models.TextField(max_length=254, null=True, verbose_name='Полное описание')
+    status = models.ForeignKey(to=Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(to=Type, on_delete=models.PROTECT)
+    created_date = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
+    updated_date = models.DateTimeField(verbose_name='Дата и время обновления', auto_now=True)
